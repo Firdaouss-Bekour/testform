0d8fef3bee529ae134479615a84333ab80232adf
Firdaouss-Bekour <firdaouss.bekour@usmba.ac.ma>
2022-11-30 21:46:33 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace testform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.livreDataSet = new testform.livreDataSet();
+            this.biblioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.biblioTableAdapter = new testform.livreDataSetTableAdapters.biblioTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prixDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.livreDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.biblioBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Ajouter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomDataGridViewTextBoxColumn,
+            this.prixDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.biblioBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 261);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(659, 295);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prix";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(145, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 62);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Modifier";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Modifier);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 62);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Supprimer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Supprimer);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(411, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 62);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Enregistrer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Enregistrer);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(544, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 62);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Annuler";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 26);
+            this.textBox3.TabIndex = 10;
+            // 
+            // livreDataSet
+            // 
+            this.livreDataSet.DataSetName = "livreDataSet";
+            this.livreDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // biblioBindingSource
+            // 
+            this.biblioBindingSource.DataMember = "biblio";
+            this.biblioBindingSource.DataSource = this.livreDataSet;
+            // 
+            // biblioTableAdapter
+            // 
+            this.biblioTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nomDataGridViewTextBoxColumn
+            // 
+            this.nomDataGridViewTextBoxColumn.DataPropertyName = "nom";
+            this.nomDataGridViewTextBoxColumn.HeaderText = "nom";
+            this.nomDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nomDataGridViewTextBoxColumn.Name = "nomDataGridViewTextBoxColumn";
+            this.nomDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // prixDataGridViewTextBoxColumn
+            // 
+            this.prixDataGridViewTextBoxColumn.DataPropertyName = "prix";
+            this.prixDataGridViewTextBoxColumn.HeaderText = "prix";
+            this.prixDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.prixDataGridViewTextBoxColumn.Name = "prixDataGridViewTextBoxColumn";
+            this.prixDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 568);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.livreDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.biblioBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private livreDataSet livreDataSet;
+        private System.Windows.Forms.BindingSource biblioBindingSource;
+        private livreDataSetTableAdapters.biblioTableAdapter biblioTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prixDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace testform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool ajoutWasClicked = false;
+        private bool modWasClicked = false;
+        private void button_Ajouter(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into biblio values('" + int.Parse(textBox3.Text) + "', '" + textBox1.Text + "', '" + int.Parse(textBox2.Text) + "')", con);
+            MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Ajouté avec success");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+           
+
+
+
+
+
+
+        }
+
+        private void button_Modifier(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update biblio set nom='" + textBox1.Text + "',prix= '" + int.Parse(textBox2.Text) + "' where id='" + int.Parse(textBox3.Text) + "'", con);
+            MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Modifié avec success");
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.Message);
+            }
+            con.Close();
+           
+        }
+
+        private void button_Supprimer(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete from biblio  where id='" + int.Parse(textBox3.Text) + "'", con);
+            MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Supprimé avec success");
+            }
+            catch (Exception ex2)
+            {
+                MessageBox.Show(ex2.Message);
+            }
+            con.Close();
+            
+        }
+
+        private void button_Enregistrer(object sender, EventArgs e)
+        {
+            if (ajoutWasClicked)
+            {
+                SqlCommand cmd = new SqlCommand("insert into biblio values('" + int.Parse(textBox3.Text) + "', '" + textBox1.Text + "', '" + int.Parse(textBox2.Text) + "')");
+                cmd.ExecuteNonQuery();
+                ajoutWasClicked = false;
+            }
+
+            if (modWasClicked)
+            {
+                SqlCommand cmd = new SqlCommand("update biblio set nom='" + textBox1.Text + "',prix= '" + int.Parse(textBox2.Text) + "' where id='" + int.Parse(textBox3.Text) + "'");
+                cmd.ExecuteNonQuery();
+                modWasClicked = false;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'livreDataSet.biblio' table. You can move, or remove it, as needed.
+            this.biblioTableAdapter.Fill(this.livreDataSet.biblio);
+          
+        }
+
+    }
+}

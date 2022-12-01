bf3c18a16610546536580d15aec7bd158c3c7673
Firdaouss-Bekour <116847659+Firdaouss-Bekour@users.noreply.github.com>
2022-12-01 20:03:07 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,12 +19,12 @@ namespace testform
         {
             InitializeComponent();
         }
-
         private bool ajoutWasClicked = false;
         private bool modWasClicked = false;
         private void button_Ajouter(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
+
+            /* con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
             con.Open();
             SqlCommand cmd = new SqlCommand("insert into biblio values('" + int.Parse(textBox3.Text) + "', '" + textBox1.Text + "', '" + int.Parse(textBox2.Text) + "')", con);
             MessageBox.Show(cmd.CommandText);
@@ -37,12 +37,16 @@ namespace testform
             {
                 MessageBox.Show(ex.Message);
             }
-            con.Close();
-           
+            con.Close();*/
 
-
-
-
+            button5.Enabled = true;
+            button4.Enabled = true;
+            button2.Enabled = false;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
 
 
         }
@@ -63,7 +67,16 @@ namespace testform
                 MessageBox.Show(ex1.Message);
             }
             con.Close();
-           
+
+            button5.Enabled = false;
+            button4.Enabled = true;
+            button2.Enabled = false;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+
         }
 
         private void button_Supprimer(object sender, EventArgs e)
@@ -82,33 +95,100 @@ namespace testform
                 MessageBox.Show(ex2.Message);
             }
             con.Close();
-            
+
+            button5.Enabled = false;
+            button4.Enabled = false;
+            button2.Enabled = false;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+
         }
 
         private void button_Enregistrer(object sender, EventArgs e)
         {
-            if (ajoutWasClicked)
-            {
-                SqlCommand cmd = new SqlCommand("insert into biblio values('" + int.Parse(textBox3.Text) + "', '" + textBox1.Text + "', '" + int.Parse(textBox2.Text) + "')");
-                cmd.ExecuteNonQuery();
-                ajoutWasClicked = false;
-            }
 
-            if (modWasClicked)
-            {
-                SqlCommand cmd = new SqlCommand("update biblio set nom='" + textBox1.Text + "',prix= '" + int.Parse(textBox2.Text) + "' where id='" + int.Parse(textBox3.Text) + "'");
-                cmd.ExecuteNonQuery();
-                modWasClicked = false;
 
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-U783UJF\\SQLEXPRESS;Initial Catalog=livre;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into biblio values('" + int.Parse(textBox3.Text) + "', '" + textBox1.Text + "', '" + int.Parse(textBox2.Text) + "')", con);
+            MessageBox.Show(cmd.CommandText);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Ajouté avec success");
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+
+
+            button5.Enabled = false;
+            button4.Enabled = false;
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'livreDataSet.biblio' table. You can move, or remove it, as needed.
             this.biblioTableAdapter.Fill(this.livreDataSet.biblio);
-          
+
+            button5.Enabled = false;
+            button4.Enabled = false;
+            button2.Enabled = false;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+
         }
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (modWasClicked)
+            {
+
+                
+
+                button5.Enabled = false;//anuuler
+                button4.Enabled = false;//
+                button2.Enabled = true;
+                button1.Enabled = true;
+                button3.Enabled = true;
+                textBox3.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                
+
+            }
+            else
+            {
+
+
+
+                button5.Enabled = false;
+                button4.Enabled = false;
+                button2.Enabled = false;
+                button1.Enabled = true;
+                button3.Enabled = false;
+                textBox3.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Clear();
+                textBox2.Enabled = false;
+                textBox1.Clear();
+                textBox3.Clear();
+
+            }
+        }
     }
 }
